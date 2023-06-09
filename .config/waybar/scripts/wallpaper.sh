a9b4b04371a34b25a6ec9ff9360be9b712555782
Richard William <rwilliam.flores@gmail.com>
2023-06-09 13:21:18 +0800

    updated configs and added wallpapers

--- .config/waybar/scripts/wallpaper.sh
+++ .config/waybar/scripts/wallpaper.sh
@@ -12,7 +12,7 @@ if [[ $(pidof swww) ]]; then
 fi
 
 # swww init
-swww img ${DIR}/${RANDOMPICS} --transition-type grow --transition-fps 60 --transition-duration 1.0 --transition-pos 0.768,0.970 --transition-bezier 0.65,0,0.35,1 --transition-step 255
+swww img ${DIR}/${RANDOMPICS} --transition-type grow --transition-fps 60 --transition-duration 1.0 --transition-pos 0.805,0.970 --transition-bezier 0.65,0,0.35,1 --transition-step 255
 wal -i ${DIR}/${RANDOMPICS} --cols16
 pywalfox update
 pywal-discord -t default
