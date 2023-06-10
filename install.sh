e46eec83baef93c83088bee667e80a607905d9f0
Richard William <rwilliam.flores@gmail.com>
2023-06-10 17:46:41 +0800

    added install script (wip)

---
+++ install.sh
@@ -0,0 +1,24 @@
+echo "Installing rchrdwllm's dotfiles..."
+echo "Copying .config and Wallpapers to /home directory..."
+
+cp -r .config $HOME
+cp -r Wallpapers $HOME
+
+echo "Successfully copied files!"
+echo "Copying SDDM theme to /usr/share/sddm/themes..."
+echo "Your password is needed for this one"
+
+sudo cp -r usr/share/sddm/themes/sugar-candy/* /usr/share/sddm/themes/sugar-candy/
+sudo cp -r lib/sddm/sddm.conf.d/default.conf /lib/sddm/sddm.conf.d/
+
+echo "Successfully copied SDDM theme!"
+echo "You can now reboot your system to see changes"
+echo "Would you like to reboot now? (y/n)"
+
+read reboot
+
+if [ $reboot = "y" ]; then
+    sudo reboot
+else
+    echo "Alright, you can reboot later by typing 'sudo reboot'"
+fi
