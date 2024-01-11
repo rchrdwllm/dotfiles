80c19d359128fed8f32e373693727ac36def7cd4
Richard William <rwilliam.flores@gmail.com>
2024-01-11 16:10:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 echo "Installing rchrdwllm's dotfiles..."
+echo "Installing base Hyprland packages..."
+
+paru -S wayland libdrm pixman libxkbcommon python libxml2 llvm libpng gegl mtdev xorg-xwayland qt5-wayland qt6-wayland hyprland swww waybar xdg-desktop-portal-wlr wlroots xdg-desktop-portal
+paru -S google-chrome discord betterdiscordctl betterdiscord-git telegram-desktop cider visual-studio-code-bin obs-studio vlc
+
+echo "Installing Oh My Zsh..."
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 echo "Copying .config and Wallpapers to /home directory..."
 
 cp -r .config $HOME
