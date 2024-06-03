807b8a243a0aeabe98fedf6c99d65c9627f3b1db
Richard William <rwilliam.flores@gmail.com>
2024-06-03 14:57:09 +0800

    Updated pywal stuff

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ makepkg -si
 
 cd ../
 
-paru -S wayland kitty libdrm pixman libxkbcommon python libxml2 llvm libpng gegl mtdev xorg-xwayland qt5-wayland qt6-wayland hyprland swww waybar xdg-desktop-portal-wlr wlroots xdg-desktop-portal sddm
+paru -S wayland kitty libdrm pixman libxkbcommon python libxml2 llvm libpng gegl mtdev xorg-xwayland qt5-wayland qt6-wayland hyprland swww waybar xdg-desktop-portal-wlr wlroots xdg-desktop-portal sddm sddm-sugar-dark
 paru -S zsh polkit-kde-agent pcmanfm-qt neovim gedit swaylock-effects brightnessctl pavucontrol alsa-utils grim slurp wl-clipboard mpv python-pip rofi blueberry bluez bluez-utils ranger ts-node zsh ttf-jetbrains-mono ttf-jetbrains-mono-nerd inotify-tools thunar ark playerctl pamixer whitesur-icon-theme-git whitesur-cursor-theme-git whitesur-gtk-theme-git xdg-user-dirs nwg-look-bin python-pillow python-pywalfox pywal-discord-git mako-git viewnior gnome-keyring neofetch imagemagick wtype inter-font rofi-emoji nodejs bun-bin jdk-openjdk noto-fonts-emoji ttf-droid alsa-firmware tumbler wal-telegram-git
 paru -S firefox-developer-edition discord betterdiscordctl betterdiscord-git telegram-desktop code code-features obs-studio vlc
 
@@ -28,25 +28,27 @@ echo "Installing Pywal..."
 git clone https://github.com/eylles/pywal16 ~/Downloads/Programs/pywal16
 pip3 install --user ~/Downloads/Programs/pywal16 --break-system-packages
 
-cd $HOME/.cache/wal
+cd $HOME/.cache
+mkdir wal
+cd wal
 touch mode
 echo "dark" >| ${HOME}/.cache/wal
 
 ~/.config/waybar/scripts/wallpaper.sh
 
-echo "Copying zsh files..."
-
-cp .zshrc $HOME
-cp .p10k.zsh $HOME
-
-echo "Successfully copied files!"
 echo "Enabling services SDDM and bluetooth services..."
 
 systemctl enable sddm.service
 systemctl enable bluetooth.service
 
 echo "Successfully enabled services!"
+echo "Copying zsh files..."
 
+cd $HOME/Downloads/Programs/dotfiles
+cp .zshrc $HOME
+cp .p10k.zsh $HOME
+
+echo "Successfully copied files!"
 echo "You can now reboot your system to see changes"
 echo "Would you like to reboot now? (y/n)"
 
